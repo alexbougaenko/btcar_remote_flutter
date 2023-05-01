2ac73ae7dd0a31c875f2821bce7d5dfe6e6e0af2
Alex Bougaenko <alexbougaenko@gmail.com>
2023-05-01 18:56:52 +0600

    basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'BTCar Remote',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'BTCar Remote'),
     );
   }
 }
@@ -49,6 +49,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  List<String> _options = ['Option 1', 'Option 2', 'Option 3'];
+  int _selectedOption = 0;
 
   void _incrementCounter() {
     setState(() {
@@ -95,20 +97,39 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            DropdownButton<String>(
+              value: _options[_selectedOption],
+              onChanged: (String? newValue) {
+                setState(() {
+                  _selectedOption = _options.indexOf(newValue!);
+                });
+              },
+              items: _options.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Refresh'),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Connect'),
+                ),
+              ],
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Not connected',
+              style: Theme.of(context).textTheme.headlineSmall,
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
